06cb6af1d2344db7e7f0b4d4117688ff76f26a03
eliran <eliranovadia7@gmail.com>
2025-10-21 11:32:07 +0300

    we now have a skeleton with a page factory with an explicit page instances.

--- tests/test_login.py
+++ tests/test_login.py
@@ -7,23 +7,7 @@ from src.page_factory import PageFactory
 @pytest.mark.usefixtures("set_web_driver")
 class TestLogin:
 
-    def xtest_reach_demo(self, set_web_driver):
-        dashboard = LoginFlow(set_web_driver).login_with_email_and_password()
-        dashboard.enter_demo()
-
-    def xtest_reach_trade(self, set_web_driver):
-        dashboard = LoginFlow(set_web_driver).login_with_email_and_password()
-        dashboard.nav_trade()
-
-    def xtest_enter_profile(self, set_web_driver):
-        dashboard = LoginFlow(set_web_driver).login_with_email_and_password()
-        dashboard.enter_profile()
-
-    def xtest_enter_currency_exchange(self, set_web_driver):
-        dashboard = LoginFlow(set_web_driver).login_with_email_and_password()
-        dashboard.enter_currency_exchange()
-
-    #@pytest.mark.smoke
+    @pytest.mark.smoke
     def test_moving_between_pages(self, set_web_driver):
         pf = PageFactory(set_web_driver)
         dashboard = LoginFlow(set_web_driver, pf).login_with_email_and_password().nav_trade()
