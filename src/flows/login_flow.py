e3d5f5252437aa3d9fa62cb2d955564ffd73a58a
eliran <elirano@ibi.co.il>
2026-02-25 10:55:59 +0200

    removed page factory

--- src/flows/login_flow.py
+++ src/flows/login_flow.py
@@ -1,38 +1,44 @@
 from src.flows.base_flow import BaseFlow
-from src.page_factory import PageFactory
+
+from src.pages.welcome_page import WelcomePage
+from src.pages.dashboard_page import DashboardPage
+from src.pages.login_with_password_page import LoginWithPasswordPage
+from src.pages.otp_page import OtpPage
+from src.pages.portfolio_page import PortfolioPage
+from src.pages.terms_page import TermsPage
+
 
 class LoginFlow(BaseFlow):
 
-    def __init__(self, driver, factory: PageFactory | None = None):
+    def __init__(self, driver):
         super().__init__(driver)
         self.driver = driver
         self.platform = (driver.capabilities.get("platformName") or "").lower()
-        self.pf = factory or PageFactory(driver)
 
     def login_with_credentials(self):
         if self.platform == "android":
-            terms = self.pf.terms()
+            terms = TermsPage(self.driver)
             terms.accept_terms()
 
-        welcome = self.pf.welcome()
+        welcome = WelcomePage(self.driver)
         welcome.enter_username()
         welcome.tap_welcome_login()
 
-        otp = self.pf.otp()
+        otp = OtpPage(self.driver)
         otp.tap_login_with_password()
 
-        password_login = self.pf.login_with_password()
+        password_login = LoginWithPasswordPage(self.driver)
         password_login.enter_password()
         password_login.tap_password_login()
 
-        dashboard = self.pf.dashboard()
+        dashboard = DashboardPage(self.driver)
         return dashboard
 
     def enter_trade_with_credentials(self):
         dashboard = self.login_with_credentials()
         dashboard.nav_trade_portfolio()
 
-        portfolio = self.pf.portfolio()
+        portfolio = PortfolioPage(self.driver)
         return portfolio
 
 
--- tests/test_login.py
+++ tests/test_login.py
@@ -2,8 +2,8 @@ from time import sleep
 
 import pytest
 
+from src.pages.dashboard_page import DashboardPage
 from src.flows.login_flow import LoginFlow
-from src.page_factory import PageFactory
 
 
 @pytest.mark.usefixtures("set_web_driver")
@@ -11,10 +11,8 @@ class TestTrading:
 
     @pytest.mark.smoke
     def test_moving_between_pages(self, set_web_driver):
-        pf = PageFactory(set_web_driver)
-
 
         login_flow = LoginFlow(set_web_driver).login_with_credentials()
-        dashboard = pf.dashboard().nav_trade_portfolio()
+        dashboard = DashboardPage(set_web_driver).nav_trade_portfolio()
 
         sleep(5)
